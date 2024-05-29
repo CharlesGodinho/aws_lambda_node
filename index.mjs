ea1acd4e7524448de85df7f1c021bab28a6a4f7b
CharlesGodinho <charleshenriquesouza@gmail.com>
2024-05-29 15:02:28 -0300

    funcao API

--- index.mjs
+++ index.mjs
@@ -4,7 +4,10 @@ export const handler = async (event) => {
     log(' event: ' +JSON.stringify(event));
     return {
       statusCode: 200,
-      body: JSON.stringify(event),
+      body: '<html><body> Dados da requisicao '+JSON.stringify(event) +'</body></html>',
+      headers: {
+        "content-type": "text/html"
+      }
     };
   };
   
